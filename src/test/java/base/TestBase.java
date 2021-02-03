6b9003ae64e530a539044182ee98d283a9e35c2a
hannapiestova <hannapiestova95@gmail.com>
2021-02-03 12:23:16 -0600

    made change

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -1,5 +1,18 @@
 package base;
 
+import org.openqa.selenium.WebDriver;
+import utilities.ConfigurationReader;
+import utilities.Driver;
+
 public abstract class TestBase {
-    
+
+    public WebDriver driver = Driver.get();
+
+
+    public void setUpMethod(){
+
+        Driver.get().get(ConfigurationReader.getProperty("url"));
+
+
+    }
 }
