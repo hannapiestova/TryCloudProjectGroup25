85d52e086bfd24b98b281968baf3bc852344d170
vladyslavpaliy <vladyslavpaliy@gmail.com>
2021-02-06 15:45:16 -0600

    Test 7 and 8 done

--- src/test/java/tests/US3.java
+++ src/test/java/tests/US3.java
@@ -15,6 +15,7 @@ import utilities.ConfigurationReader;
 import utilities.Driver;
 import utilities.TryCloudUtil;
 
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 public class US3 {
@@ -24,21 +25,22 @@ public class US3 {
     // // // new contact WebElement under Contact module
 
     @BeforeClass
-    public void setUp(){
+    public void setUp() {
         Driver.get().get(ConfigurationReader.getProperty("url1"));
         TryCloudUtil.LogInTryCloud(driver);
     }
 
 
     @BeforeMethod
-    public void sleep(){
+    public void sleep() {
         BrowserUtils.wait(1);
     }
 
 
     @Test(priority = 1)
-    public void TC1(){
+    public void TC1() {
         //Test case #1 - verify users can access to Files module
+
         //1. Login as a user //
         // TryCloudUtil.LogInTryCloud(driver);
         driver.findElement(By.xpath("//*[@id=\"appmenu\"]/li[2]/a")).click();
@@ -47,9 +49,11 @@ public class US3 {
         // Asserts method to verificate title and String message.
         Assert.assertTrue(actualTitleFiles.contains("Files"), "Verification title Failed");
     }
+
     @Test(priority = 2)
     public void TC2() throws InterruptedException {
         //Test case #2 - verify users can select all the uploaded files from the page
+
         //1. Login as a user
         // TryCloudUtil.LogInTryCloud(driver);
         // go to Files
@@ -58,74 +62,91 @@ public class US3 {
         //2. Click the top left checkbox of the table
         driver.findElement(By.xpath("//*[@id=\"headerSelection\"]/label")).click();
         //3. Assert all the files are selected
-        boolean selected1 = driver.findElement(By.xpath("//*[@id=\"fileList\"]/tr[1]/td[1]/label")).isSelected();
-        boolean selected2 = driver.findElement(By.xpath("//*[@id=\"fileList\"]/tr[2]/td[1]/label")).isSelected();
+        WebElement checkbox1 = driver.findElement(By.xpath("//*[@id=\"fileList\"]/tr[1]/td[1]/label"));
+        WebElement checkbox2 = driver.findElement(By.xpath("//*[@id=\"fileList\"]/tr[2]/td[1]/label"));
         BrowserUtils.wait(1);
-        Assert.assertTrue(selected1,"Not selected");
-        Assert.assertTrue(selected2,"Not selected");
-        //fail
+
+        if (checkbox1.isSelected()) {
+            System.out.println("Checkbox#1 is selected. PASSED!");
+        } else {
+            System.out.println("Checkbox#1 NOT selected. FAILED!");
+        }
+
+        if (checkbox2.isSelected()) {
+            System.out.println("Checkbox#2 is selected. PASSED!!!");
+        } else {
+            System.out.println("Checkbox#2 NOT selected. FAILED!");
+        }
+
+
+        BrowserUtils.wait(1);
+        driver.navigate().refresh();
+        BrowserUtils.wait(1);
 
         //(Pre-condition: there should be at least 2 files are uploaded the page)
     }
 
     @Test(priority = 3)
     public void TC3() {
-
         //Test case #3 - verify users can add a file to favorites.
+
         //1. Login as a user
         // TryCloudUtil.LogInTryCloud(driver);
-         driver.findElement(By.xpath("//*[@id=\"appmenu\"]/li[2]/a")).click();
+        driver.findElement(By.xpath("//*[@id=\"appmenu\"]/li[2]/a")).click();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         //2. Click action-icon from any file on the page
-         driver.findElement(By.xpath("//*[@id=\"fileList\"]/tr[1]/td[2]/a/span[3]/a[2]")).click();
+        driver.findElement(By.xpath("//*[@id=\"fileList\"]/tr[1]/td[2]/a/span[3]/a[2]")).click();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         //3. Choose "Add to Favorites" option
-         driver.findElement(By.xpath("//*[@id=\"fileList\"]/tr[1]/td[2]/div/ul/li[3]/a/span[2]")).click();
+        driver.findElement(By.xpath("//*[@id=\"fileList\"]/tr[1]/td[2]/div/ul/li[3]/a/span[2]")).click();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         //4. Click "Favorites" sub-module on the lest side
-         driver.findElement(By.xpath("//*[@id=\"app-navigation\"]/ul/li[3]/a")).click(); // Click "Favorites"
+        driver.findElement(By.xpath("//*[@id=\"app-navigation\"]/ul/li[3]/a")).click(); // Click "Favorites"
         BrowserUtils.wait(1);
         //5. Verify the chosen file is listed on the table
-         WebElement newFile = driver.findElement(By.xpath("/html/body/div[3]/div[2]/div[4]/table/tbody/tr/td[1]/a/span[1]"));
+        WebElement newFile = driver.findElement(By.xpath("/html/body/div[3]/div[2]/div[4]/table/tbody/tr/td[1]/a/span[1]"));
         BrowserUtils.wait(1);
         //(Pre-condition: there should be at least 1 file is uploaded)
-        Assert.assertTrue(newFile.isDisplayed(),"FAIL, File not displayed");
+        Assert.assertTrue(newFile.isDisplayed(), "FAIL, File not displayed");
         BrowserUtils.wait(1);
-
-        // Remove "Favorites tag" by clicking right mouse, if we not gonna remove next time Test gonna fail
-        // creating Action
-        // Actions action = new Actions(driver);
-        // action.contextClick(newFile).click(); right mouse click
-         driver.findElement(By.xpath("//*[@id=\"fileList\"]/tr/td[1]/a/span[3]/a[2]")).click();
-         driver.findElement(By.xpath("//*[@id=\"fileList\"]/tr/td[1]/div/ul/li[3]/a/span[2]")).click();
-         // refresh page
-        BrowserUtils.wait(1);
-        driver.navigate().refresh();
-
 
 
     }
+
     @Test(priority = 4)
     public void TC4() {
-        // NOT POSSIBLE TO TEST NO ICON  "Remove from Favorites"
-
         //Test case #4 - verify users can remove a file from favorites.
+
         //1. Login as a user
         // TryCloudUtil.LogInTryCloud(driver);
-    //    driver.findElement(By.xpath("//*[@id=\"appmenu\"]/li[2]/a")).click();
+        driver.findElement(By.xpath("//*[@id=\"appmenu\"]/li[2]/a")).click();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         //2. Click action-icon from any file on the page
-    //    driver.findElement(By.xpath("//*[@id=\"fileList\"]/tr[1]/td[1]/label")).click();
+        driver.findElement(By.xpath("//*[@id=\"fileList\"]/tr[1]/td[2]/a/span[3]/a[2]")).click();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         //3. Click "Remove from Favorites" option
-        //  no  ""Remove from Favorites""
+        driver.findElement(By.xpath("//*[@id=\"fileList\"]/tr[1]/td[2]/div/ul/li[3]/a/span[2]")).click();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         //4. Verify that the file is removed from Favorites sub-module's table.
+        driver.findElement(By.xpath("//*[@id=\"app-navigation\"]/ul/li[3]/a")).click(); // Click "Favorites"
+        BrowserUtils.wait(1);
+
+        WebElement newFile = driver.findElement(By.xpath("/html/body/div[3]/div[2]/div[4]/table/tbody/tr/td[1]/a/span[1]"));
+        BrowserUtils.wait(1);
+        Assert.assertFalse(newFile.isDisplayed(), "FAIL file still here!");
         //(Pre-condition: there should be at least 1 file is added to favorites table)
 
+        BrowserUtils.wait(1);
+        driver.navigate().refresh();
+        BrowserUtils.wait(1);
     }
+
     @Test(priority = 5)
     public void TC5() {
-    //Test case #5 - verify users can upload a file directly to the homepage (File-Uploading)
-    // 1. Login as a user
-    // TryCloudUtil.LogInTryCloud(driver);
+        //Test case #5 - verify users can upload a file directly to the homepage (File-Uploading)
+
+        // 1. Login as a user
+        // TryCloudUtil.LogInTryCloud(driver);
         // go to Files
 
         // path of file
@@ -133,74 +154,82 @@ public class US3 {
 
         Driver.get().findElement(By.xpath("//*[@id=\"appmenu\"]/li[2]/a")).click();
         BrowserUtils.wait(1);
-    // 2. Click the "+" icon on top
+        // 2. Click the "+" icon on top
         Driver.get().findElement(By.xpath("//*[@id=\"controls\"]/div[2]/a")).click();
         BrowserUtils.wait(1);
-    //3. Click "upload file"
+        //3. Click "upload file"
         WebElement chooseFile = driver.findElement(By.xpath("//*[@id=\"controls\"]/div[2]/div[2]/ul/li[1]/label"));
         BrowserUtils.wait(1);
-    //4. Upload a file
+        //4. Upload a file
         chooseFile.sendKeys(filePatch);
         // fail
 
-    //5. Verify the file is displayed on the page
+        //5. Verify the file is displayed on the page
 
     }
 
     @Test(priority = 6)
     public void TC6() {
-//Test case #6 - verify users can create a folder
-//1. Login as a user
+        //Test case #6 - verify users can create a folder
+
+        //1. Login as a user
         // go to Files
         Driver.get().findElement(By.xpath("//*[@id=\"appmenu\"]/li[2]/a")).click();
         BrowserUtils.wait(1);
-//2. Click the "+" icon on top
+        //2. Click the "+" icon on top
         Driver.get().findElement(By.xpath("//*[@id=\"controls\"]/div[2]/a")).click();
         BrowserUtils.wait(1);
-//3. Click "New Folder"
+        //3. Click "New Folder"
         Driver.get().findElement(By.xpath("//*[@id=\"controls\"]/div[2]/div[2]/ul/li[2]/a")).click();
         BrowserUtils.wait(1);
-//4. Write a folder name
+        //4. Write a folder name
         Driver.get().findElement(By.xpath("//*[@id=\"view13-input-folder\"]")).clear();
         BrowserUtils.wait(1);
         String nameOfFolder = faker.name().firstName();
-        Driver.get().findElement(By.xpath("//*[@id=\"view13-input-folder\"]")).sendKeys(nameOfFolder+Keys.ENTER);
+        Driver.get().findElement(By.xpath("//*[@id=\"view13-input-folder\"]")).sendKeys(nameOfFolder + Keys.ENTER);
         BrowserUtils.wait(1);
-//5. Click submit icon
-       //  Driver.get().findElement(By.xpath("//*[@id=\"view13-input-folder\"]")).sendKeys("Folder name"+Keys.ENTER);
-//6. Verify the folder is displayed on the page
+        //5. Click submit icon
+        //  Driver.get().findElement(By.xpath("//*[@id=\"view13-input-folder\"]")).sendKeys("Folder name"+Keys.ENTER);
+        //6. Verify the folder is displayed on the page
 
         //fail
     }
+
     @Test(priority = 7)
     public void TC7() {
+        //Test case #7 - verify users can upload a file inside a folder
 
-//Test case #7 - verify users can upload a file inside a folder
-//1. Login as a user
+        //1. Login as a user
         // go to Files
         Driver.get().findElement(By.xpath("//*[@id=\"appmenu\"]/li[2]/a")).click();
         BrowserUtils.wait(1);
-//2. Choose a folder from the page
+        //2. Choose a folder from the page
         Driver.get().findElement(By.xpath("//*[@id=\"fileList\"]/tr[2]/td[2]/a/span[1]/span")).click();
-//3. Click the "+" icon on top
+        BrowserUtils.wait(1);
+        //3. Click the "+" icon on top
         Driver.get().findElement(By.xpath("//*[@id=\"controls\"]/div[2]/a")).click();
-//4. Click "upload file"
+        BrowserUtils.wait(1);
+        //4. Click "upload file"
         String filePatch = "/Users/vladyslavpaliy/Desktop/Screen Shot 2021-02-05 at 15.16.28xzdsa.png";
         WebElement chooseFile = driver.findElement(By.xpath("//*[@id=\"controls\"]/div[2]/div[2]/ul/li[1]/label/span[2]"));
-//5. Upload a file
+        //5. Upload a file
+        BrowserUtils.wait(1);
         chooseFile.click();
         chooseFile.sendKeys(filePatch);
 
+
         // fail
 
-//6. Verify the file is displayed on the page
-//(Pre-condition: there should be at least 1 folder is created on the filers page)
+        //6. Verify the file is displayed on the page
+        //(Pre-condition: there should be at least 1 folder is created on the filers page)
 
     }
+
     @Test(priority = 8)
     public void TC8() {
-//Test case #8 - verify users can delete a file/folder.
-//1. Login as a user
+        //Test case #8 - verify users can delete a file/folder.
+
+        //1. Login as a user
         // go to Files
         Driver.get().findElement(By.xpath("//*[@id=\"appmenu\"]/li[2]/a")).click();
         BrowserUtils.wait(1);
@@ -211,19 +240,51 @@ public class US3 {
         newFile.click();
         WebElement nameOfFile = Driver.get().findElement(By.xpath("//*[@id=\"view13-input-x-office-document\"]"));
         nameOfFile.clear();
-        nameOfFile.sendKeys("AFile"+Keys.ENTER);
-
-//2. Click action-icon from any file on the page
-Driver.get().findElement(By.xpath("//*[@id=\"fileList\"]/tr[4]/td[2]/a/span[2]/a[2]")).click();
-//3. Choose "delete files" option
-//4. Click deleted files on the left bottom corner
+        nameOfFile.sendKeys("aFile" + Keys.ENTER);
+        BrowserUtils.wait(1);
+        //2. Click action-icon from any file on the page
+        Driver.get().findElement(By.xpath("//*[@id=\"fileList\"]/tr[4]/td[2]/a/span[2]/a[2]")).click();
+        BrowserUtils.wait(1);
+        //3. Choose "delete files" option
+        //4. Click deleted files on the left bottom corner
         Driver.get().findElement(By.xpath("//*[@id=\"fileList\"]/tr[4]/td[2]/div/ul/li[8]/a/span[2]")).click();
-//5. Verify the deleted file is displayed on the page.
+        BrowserUtils.wait(1);
+        //5. Verify the deleted file is displayed on the page.
 
-//(Pre-condition: there should be at least 1 file is uploaded inside files page)
+
+       // WebElement newFileCreated = Driver.get().findElement(By.xpath("//*[@id=\"fileList\"]/tr[4]/td[2]/a/span[1]"));
+        //(Pre-condition: there should be at least 1 file is uploaded inside files page)
+
+
+        // FAIL
+
+
+        try {
+            WebElement newFileCreated = Driver.get().findElement(By.xpath("//*[@id=\"fileList\"]/tr[4]/td[2]/a/span[1]"));
+
+            if (newFileCreated.isDisplayed()) {
+                System.out.println("aFile deleted, test PASS");
+            }
+        }catch (NoSuchElementException e){
+            System.out.println("aFile exist, FAIL");
+        }
+
+
+
+
+      /*
+        if (newFileCreated.isDisplayed()) {
+            System.out.println("aFile deleted, test PASS");
+        } else {
+            System.out.println("aFile exist, FAIL");
+        }
+*/
+        BrowserUtils.wait(1);
+        driver.navigate().refresh();
+        BrowserUtils.wait(1);
 
     }
 
-   // @AfterClass
-  //  public void close(){driver.quit();}
-}
+        // @AfterClass
+        //  public void close(){driver.quit();}
+    }
