f9fbd9e27567a7e430015a32d863d717fab79419
Sunnat Davletov <Davletovnyc@gmail.com>
2021-02-08 00:37:38 -0500

    Merge branch 'master' into US4
    # Conflicts:
    #	target/test-classes/base/Test.class
    #	target/test-classes/tests/Test1.class
    #	target/test-classes/utilities/BrowserUtils.class
    #	target/test-classes/utilities/ConfigurationReader.class
    #	target/test-classes/utilities/Driver.class
    #	target/test-classes/utilities/WebDriverFactory.class

--- src/test/java/tests/US4.java
+++ src/test/java/tests/US4.java
@@ -16,26 +16,19 @@ import utilities.Driver;
 import utilities.TryCloudUtil;
 
 public class US4 {
-
     WebDriver driver = Driver.get();
     Faker faker = new Faker();
 
-    @BeforeClass
-    public void setUp() {
-        Driver.get().get(ConfigurationReader.getProperty("url"));
-        TryCloudUtil.LogInTryCloud(driver);
-    }
-
     @BeforeMethod
-    public void sleep() {
-        BrowserUtils.wait(3);
+    public void setUp() {
+        Driver.get().get(ConfigurationReader.getProperty("url0"));
+        TryCloudUtil.LogInTryCloud(driver);
     }
 
     @Test(priority = 1)
     public void TC1_verification_title() {
         //2. Click Talks module
-        WebElement TalksModule = driver.findElement(By.xpath("(//*[@aria-label='Talk'])[1]"));
-        TalksModule.click();
+        Driver.get().findElement(By.xpath("(//*[@aria-label='Talk'])[1]")).click();
         //3. Verify the page tile is Talks module's tile
         String actualPageTitle = driver.getTitle();
         String expectedPageTitle = "Talk - Trycloud QA";
@@ -45,24 +38,21 @@ public class US4 {
     @Test(priority = 2)
     public void TC2_verification_send_message() {
         // Click Talks module
-        WebElement TalksModule = driver.findElement(By.xpath("(//*[@aria-label='Talk'])[1]"));
-        TalksModule.click();
-        BrowserUtils.wait(2);
+        Driver.get().findElement(By.xpath("(//*[@aria-label='Talk'])[1]")).click();
+        BrowserUtils.wait(1);
         //3. Search a "user" from search box on the left
         //Option
-        WebElement option = driver.findElement(By.xpath("//div//*[@class='app-navigation-toggle']"));
+        //  WebElement option = Driver.get().findElement(By.xpath("//div//*[@class='app-navigation-toggle']"));
         //search
-        WebElement searchbox = driver.findElement(By.xpath("//input[@type='text']"));
+        driver.findElement(By.xpath("//input[@type='text']")).sendKeys("user100");
         //enter user100
-        searchbox.sendKeys("user100");
-        WebElement user = driver.findElement(By.xpath("//*[@class='acli__content__line-one__title']"));
-        user.click();
+        driver.findElement(By.xpath("//*[@class='acli__content__line-one__title']")).click();
         //4. Creating a faker to write a message
         String message = faker.lorem().fixedString(5);
         //5. Click submit button
         driver.findElement(By.xpath("//div[@contenteditable='true']")).sendKeys(message + Keys.ENTER);
         //6. Verify the message is displayed on the conversation log
-        Assert.assertTrue(driver.findElement(By.xpath("//*[text()='" + message + "']")).isDisplayed());
+        Assert.assertTrue(Driver.get().findElement(By.xpath("//*[text()='" + message + "']")).isDisplayed());
     }
 
     @AfterMethod
--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -21,14 +21,14 @@ public class Driver {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
                     driver=new ChromeDriver();
-                   // driver.manage().window().maximize();
-                   // driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+                   driver.manage().window().maximize();
+                   driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
                     break;
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
                     driver=new FirefoxDriver();
-                   // driver.manage().window().maximize();
-                   // driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
                     break;
                 default:
                     throw  new RuntimeException("Wrong browser type");
